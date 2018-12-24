df3358ef9289063ca77e1cd5a1cbb537e1ecd29b
danielmbutler <46110343+danielmbutler@users.noreply.github.com>
2018-12-24 20:23:18 +0000

    Add files via upload

---
+++ web request tracker.ps1
@@ -0,0 +1,21 @@
+﻿
+
+
+
+function Get-webstats($urls){
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+foreach($url in $urls){
+     $date = Get-Date
+     Write-Host $url, "HTTP STATUS CODE" (Invoke-WebRequest -uri $url).StatusCode, $date
+
+}
+
+}
+
+
+$webreqs = Read-Host -Prompt "Provide Url(s)"
+foreach($webreq in $webreqs){
+ $date = Get-Date
+ Write-Host $link, "HTTP STATUS CODE" (Invoke-WebRequest -uri $webreq).StatusCode, $date
+
+}
